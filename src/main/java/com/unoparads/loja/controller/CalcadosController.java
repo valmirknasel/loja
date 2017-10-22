490b7d888b3724d15fa310fb3fe64a553e898099
Valmir Junior <valmirknasel@gmail.com>
2017-10-22 02:27:02 -0400

    Correções :) metodo salvar funcionando. Listar tbm :)

--- src/main/java/com/unoparads/loja/controller/CalcadosController.java
+++ src/main/java/com/unoparads/loja/controller/CalcadosController.java
@@ -5,10 +5,13 @@
  */
 package com.unoparads.loja.controller;
 
+import com.unoparads.loja.model.Calcado;
 import com.unoparads.loja.repository.Calcados;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -16,16 +19,25 @@ import org.springframework.web.servlet.ModelAndView;
  * @author StallkerTwo
  */
 @Controller
+@RequestMapping(value= "/calcados")
 public class CalcadosController {
     
     @Autowired
     private Calcados calcados;
     
-    @GetMapping("/calcados")
+    @GetMapping
     public ModelAndView listar() {
-        ModelAndView modelAndView = new ModelAndView("ListaCalcados");
-        modelAndView.addObject("calcados", calcados.findAll());
-        return modelAndView;
+        ModelAndView mv = new ModelAndView("ListaCalcados");
+        mv.addObject("calcados", calcados.findAll());
+        mv.addObject(new Calcado());
+        return mv;
+    }
+    
+    @PostMapping
+    public String salvar (Calcado calcado) {
+        this.calcados.save(calcado);
+        return "redirect:/calcados";
+        
     }
     
 }
--- src/main/java/com/unoparads/loja/model/Calcado.java
+++ src/main/java/com/unoparads/loja/model/Calcado.java
@@ -8,19 +8,24 @@ package com.unoparads.loja.model;
 import java.io.Serializable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 /**
  *
  * @author StallkerTwo
  */
 @Entity
+@Table(name = "calcado")
 public class Calcado implements Serializable{
     
     private static final long serialVersionUID = 1L;
     
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator="calcado_id_seq")
+    @SequenceGenerator(name="calcado_id_seq", sequenceName="calcado_id_seq", allocationSize=1)
     private Long id;
     
     private String nomeDoCalcado;
